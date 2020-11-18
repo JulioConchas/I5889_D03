32b9f26a15a97c4b79c61a09a9725c75586ba390
jconchas <jconchas@ibm.com>
2020-11-18 10:26:30 -0600

    numeros complejos y operaciones

---
+++ numero_complejos.py
@@ -0,0 +1,13 @@
+
+## author: Julio Conchas
+## email : conchasjiemenez@gmail.com
+
+## Numeros complejos
+a = complex(1,1)
+b = complex(1,5)
+## Suma
+print(a + b)
+## Resta
+print(a - b)
+## multiplicacion
+print(a*b)
